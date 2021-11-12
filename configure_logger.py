ca34cc41b7e752491d0a148807f9c0ce6d9009dd
estebaneb <esteban.balcarcel@invgate.com>
2021-11-12 11:38:37 -0300

    Handle logger file not found exception

--- configure_logger.py
+++ configure_logger.py
@@ -11,22 +11,20 @@ class LogManager:
     @staticmethod
     def configure_logger(filename, level=None, log_to_console=False, name="pytun", path=None):
         path = path if path is not None else LogManager.path
-        try:
-            path = join(dirname(realpath(__file__)), path)
-            os.makedirs(path)
-        except FileExistsError:
-            pass
-        except OSError:
-            path = join(dirname(realpath(__file__)), LogManager._fallback_path)
-            os.makedirs(path)
-
         level = level or logging.INFO
         logger = logging.getLogger(name)
         loggers = [logger]
         if name != "pytun":
             paramiko_log = logging.getLogger("paramiko")
             loggers.append(paramiko_log)
-        log_handler = TimedRotatingFileHandler(filename=os.path.join(path, filename), when="midnight", backupCount=30)
+        try:
+            log_handler = TimedRotatingFileHandler(filename=os.path.join(path, filename), when="midnight",
+                                                   backupCount=30)
+        except FileNotFoundError:
+            path = join(dirname(realpath(__file__)), LogManager._fallback_path)
+            os.makedirs(path)
+            log_handler = TimedRotatingFileHandler(filename=os.path.join(path, filename), when="midnight",
+                                                   backupCount=30)
         log_formatter = logging.Formatter('%(asctime)s %(process)d %(name)-12s %(levelname)-8s %(message)s')
         log_handler.setFormatter(log_formatter)
         log_handler.setLevel(level)
