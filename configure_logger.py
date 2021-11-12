7de9108222b0adbc40b967d09971823c129ec798
estebaneb <esteban.balcarcel@invgate.com>
2021-11-12 11:20:13 -0300

    Add fallback log path

--- configure_logger.py
+++ configure_logger.py
@@ -1,21 +1,32 @@
 import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from os.path import join, dirname, realpath
 
 
 class LogManager:
-
+    _fallback_path = "./logs"
     path = "./logs"
 
     @staticmethod
-    def configure_logger(filename, level=None, log_to_console=False, name="pytun"):
+    def configure_logger(filename, level=None, log_to_console=False, name="pytun", path=None):
+        path = path if path is not None else LogManager.path
+        try:
+            path = join(dirname(realpath(__file__)), path)
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        except OSError:
+            path = join(dirname(realpath(__file__)), LogManager._fallback_path)
+            os.makedirs(path)
+
         level = level or logging.INFO
         logger = logging.getLogger(name)
         loggers = [logger]
         if name != "pytun":
             paramiko_log = logging.getLogger("paramiko")
             loggers.append(paramiko_log)
-        log_handler = TimedRotatingFileHandler(filename=os.path.join(LogManager.path, filename), when="midnight", backupCount=30)
+        log_handler = TimedRotatingFileHandler(filename=os.path.join(path, filename), when="midnight", backupCount=30)
         log_formatter = logging.Formatter('%(asctime)s %(process)d %(name)-12s %(levelname)-8s %(message)s')
         log_handler.setFormatter(log_formatter)
         log_handler.setLevel(level)
