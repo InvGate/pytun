ff9070c0cec8520db164342de0e9a152487964b7
estebaneb <esteban.balcarcel@invgate.com>
2022-01-17 13:27:08 -0300

    Add comment to application path hack

--- pytun.py
+++ pytun.py
@@ -170,7 +170,8 @@ def main():
 
 
 def get_application_path():
-    if getattr(sys, 'frozen', False):
+    # hack to get the application path
+    if getattr(sys, 'frozen', False):   # check if the application is running as a bundle
         return os.path.dirname(sys.executable)
     else:
         return os.path.dirname(os.path.abspath(__file__))
